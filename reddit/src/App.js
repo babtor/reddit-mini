ad5d6d8891442918aff704b9d338ff33655d8f94
babtor <lucaswikstrom96@gmail.com>
2023-09-08 10:38:43 +0200

    working on createposts function

--- reddit/src/App.js
+++ reddit/src/App.js
@@ -1,8 +1,22 @@
 import "./App.css";
 import React from "react";
 
+export const url = () => {
+  fetch("https://dummyjson.com/posts")
+    .then((res) => res.json())
+    .then(console.log);
+};
+
+url();
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <>
+        <CreatePosts />
+      </>
+    </div>
+  );
 }
 
 export default App;
---
+++ reddit/src/components/createPost.js
@@ -0,0 +1,6 @@
+
+
+CreatePosts() {
+
+    
+}
